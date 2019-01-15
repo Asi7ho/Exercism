4ff262982909cf8e9345f6bbbcb0c3283638a3e8
Yann <yann.debain@yahoo.fr>
2019-01-15 20:36:28 +0100

    acronyme

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,16 @@
+import re
+
 def abbreviate(words):
-    pass
+    # delete special characters in the string
+    words = re.sub(r"[^a-zA-Z0-9']+", ' ', words)
+
+    # split the string
+    words = words.split(' ')
+
+    acronyme = ""
+
+    # create the acronyme
+    for word in words:
+        acronyme += word[0].upper()
+
+    return acronyme
